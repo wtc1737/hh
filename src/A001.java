ac3d9b8e17466821bfc7bcfe88be22c301692d63
wtc1737 <1737165390@qq.com>
2016-10-08 19:23:20 +0800

    本地上传

---
+++ src/A001.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class A001 {
+		//狗狗名字小黄 今年3岁 黄色
+	
+	public static void main(String[] args) {
+		Scanner io = new Scanner(System.in);
+		//循环输出小狗的名字
+		while (true) {
+			
+		System.out.println("请输入小狗的名字");
+		String go = io.next();
+
+			System.out.println("@#$%^&*!");
+		}
+		
+	}
+}
